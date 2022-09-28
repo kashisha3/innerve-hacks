d149603ca0cd6c912998c8c4520d64202d28874b
kashisha3 <113295725+kashisha3@users.noreply.github.com>
2022-09-28 13:18:52 +0530

    Add files via upload

--- sos.js
+++ sos.js
@@ -11,22 +11,21 @@ var templateParams = {
     "arorakashish311@gmail.com,kashishar311@gmail.com,deepuphogat19@gmail.com,liza24.12drosary@gmail.com",
 };
 
-// function callSOSEmail() {
-//   console.log("fn called");
-//   emailjs.send("service_8ocrxjd", "template_7c9io49", templateParams).then(
-//     function (response) {
-//       alert("SOS Message Sent!")
-//     },
-//     function (error) {
-//       alert("Please try again!")
-//     }
-//   );
-// }
+ function callSOSEmail() {
+   console.log("fn called");
+   emailjs.send("service_8ocrxjd", "template_7c9io49", templateParams).then(
+     function (response) {
+       alert("SOS Message Sent!")
+     },
+     function (error) {      alert("Please try again!")
+     }
+   );
+ }
 
 
-function callSOSEmail() {
+/*function callSOSEmail() {
     setTimeout(() => {
         alert("SOS Message Sent!")
     }, 1000);
-  }
+  }*/
 
